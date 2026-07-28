223656c46fd2dd004dc6a3f573942c6a5b0f2eb9
wuyunbboxo <oxoyuan@qq.com>
2026-07-28 20:02:17 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,15 +1,52 @@
+const TARGET = "https://render2-tdyr.onrender.com";
+
+
 export default {
-  async fetch(request, env) {
+
+  // 普通API转发
+  async fetch(request, env, ctx) {
 
     const url = new URL(request.url);
 
-    const target = "https://render2-tdyr.onrender.com";
 
-    return fetch(target + url.pathname + url.search, {
-      method: request.method,
-      headers: request.headers,
-      body: request.body
-    });
+    return fetch(
+      TARGET + url.pathname + url.search,
+      {
+        method: request.method,
+        headers: request.headers,
+
+        body:
+          request.method === "GET"
+          ? undefined
+          : request.body
+      }
+    );
+
+  },
+
+
+  // 定时保活
+  async scheduled(event, env, ctx) {
+
+    try {
+
+      const response = await fetch(TARGET);
+
+      console.log(
+        "Render2 keep alive:",
+        response.status
+      );
+
+
+    } catch (e) {
+
+      console.log(
+        "Keep alive failed:",
+        e.message
+      );
+
+    }
 
   }
+
 };
